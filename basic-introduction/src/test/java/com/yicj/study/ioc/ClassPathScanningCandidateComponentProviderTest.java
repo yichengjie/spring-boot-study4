f448afbf02bf484358d6d3c0e8feeeaeb0af24f9
易成杰 <626659321@qq.com>
2020-05-22 09:46:05 +0800

    保存

---
+++ basic-introduction/src/test/java/com/yicj/study/ioc/ClassPathScanningCandidateComponentProviderTest.java
@@ -0,0 +1,23 @@
+package com.yicj.study.ioc;
+
+import org.junit.Test;
+import org.springframework.context.annotation.ClassPathScanningCandidateComponentProvider;
+
+/**
+ * ClassName: ClassPathScanningCandidateComponentProviderTest
+ * Description: TODO(描述)
+ * Date: 2020/5/20 13:50
+ *
+ * @author yichengjie(626659321 @ qq.com)
+ * 修改记录
+ * @version 产品版本信息 yyyy-mm-dd 姓名(邮箱) 修改信息
+ */
+public class ClassPathScanningCandidateComponentProviderTest {
+
+    @Test
+    public void testFindCandidateComponents(){
+
+        //ClassPathScanningCandidateComponentProvider provider = new ClassPathScanningCandidateComponentProvider() ;
+
+    }
+}
