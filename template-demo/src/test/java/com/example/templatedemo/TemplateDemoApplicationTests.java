fd9afd43eb21698b762da853c5ad7a942cfdb5a4
易成杰 <626659321@qq.com>
2020-06-07 18:07:27 +0800

    整合nacos

---
+++ template-demo/src/test/java/com/example/templatedemo/TemplateDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.templatedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplateDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
